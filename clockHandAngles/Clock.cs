219857899e4240586e0841457f722f2365262854
BT425 <btorbert9621@gmail.com>
2014-09-09 17:57:48 -0700

    Clock Hand Angles
    Given user input of a specific time, this application returns the angles
    between the individual clock hands. This version does not yet have a
    GUI. The application runs in  a simple command window.

---
+++ clockHandAngles/Clock.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClockHandApp
+{
+    class Clock
+    {
+        public void getAllAnglesApart(ref decimal hourMinutesAngle, ref decimal minutesSecondsAngle, ref decimal hourSecondsAngle, string time)
+        {
+            int[] hoursMinutesSeconds = parseTimeString(time);
+            hourMinutesAngle = getDegreesApartHourMinuteHands(hoursMinutesSeconds);
+            minutesSecondsAngle = getDegreesApartMinutesSecondsHands(hoursMinutesSeconds);
+            hourSecondsAngle = getDegreesApartHourSecondsHands(hoursMinutesSeconds);
+        }
+
+        public void getAllHandAngles(ref decimal hourHandAngle, ref decimal minutesHandAngle, ref decimal secondsHandAngle, string time) 
+        {
+            int[] hoursMinutesSeconds = parseTimeString(time);
+            hourHandAngle = getHourHandDegrees(hoursMinutesSeconds);
+            minutesHandAngle = getMinuteHandDegrees(hoursMinutesSeconds);
+            secondsHandAngle = getSecondsHandDegrees(hoursMinutesSeconds);
+        }
+
+        private decimal getDegreesApartHourMinuteHands(int[] hoursMinutesSeconds)
+        {
+            decimal degreesApartHourMinuteHands = getSmallestPositiveAngle(getMinuteHandDegrees(hoursMinutesSeconds)
+                - getHourHandDegrees(hoursMinutesSeconds));
+            return degreesApartHourMinuteHands;
+        }
+
+        private decimal getDegreesApartMinutesSecondsHands(int[] hoursMinutesSeconds) 
+        {
+            decimal degreesApartMinuteSecondsHands = getSmallestPositiveAngle(getMinuteHandDegrees(hoursMinutesSeconds) 
+                - getSecondsHandDegrees(hoursMinutesSeconds));
+            return degreesApartMinuteSecondsHands;
+        }
+
+        private decimal getDegreesApartHourSecondsHands(int[] hoursMinutesSeconds) 
+        {
+            decimal degreesApartHourSecondsHands = getSmallestPositiveAngle(getSecondsHandDegrees(hoursMinutesSeconds)
+                - getHourHandDegrees(hoursMinutesSeconds));
+            return degreesApartHourSecondsHands;
+        }
+
+        private int[] parseTimeString(string time)
+        {
+            string[] timeVals = time.Split(':');
+            int hours = int.Parse(timeVals[0]);
+            int minutes = int.Parse(timeVals[1]);
+            int seconds = int.Parse(timeVals[2]);
+            int[] hoursMinutesSeconds = { hours, minutes, seconds };
+            return hoursMinutesSeconds;
+        }
+
+        private decimal getHourHandDegrees(int[] hoursMinutesSeconds)
+        {
+            decimal hourHandDegreesPerHour = (360m / 12m);
+            decimal hourHandDegreesPerMinute = (30m / 60m);
+            decimal hourHandDegreesPerSecond = (360m / 43200m);
+            decimal hourHandDegrees = ((decimal)hoursMinutesSeconds[0] * hourHandDegreesPerHour) 
+                + ((decimal)hoursMinutesSeconds[1] * hourHandDegreesPerMinute)
+                + ((decimal)hoursMinutesSeconds[2] * hourHandDegreesPerSecond);
+            return getSmallestPositiveAngle(hourHandDegrees);
+        }
+
+        private decimal getMinuteHandDegrees(int[] hoursMinutesSeconds) 
+        {
+            decimal minuteHandDegreesPerMinute = (360m / 60m);
+            decimal minuteHandDegressPerSecond = (360m / 3600m);
+            decimal minuteHandDegrees = ((decimal)hoursMinutesSeconds[1] * minuteHandDegreesPerMinute) 
+                + ((decimal)hoursMinutesSeconds[2] * minuteHandDegressPerSecond);
+            return getSmallestPositiveAngle(minuteHandDegrees);
+        }
+
+        private decimal getSecondsHandDegrees(int[] hoursMinutesSeconds) 
+        {             
+            decimal secondsHandDegreesPerSecond = (360m / 60m);
+            decimal secondsHandDegrees = ((decimal)hoursMinutesSeconds[2] * secondsHandDegreesPerSecond);
+            return getSmallestPositiveAngle(secondsHandDegrees);
+        }
+
+        private decimal getSmallestPositiveAngle(decimal number)
+        {
+            if (isNegative(number))
+            {
+                number = (number * -1);
+            }
+            if (isGreaterThan360(number))
+            {
+                number = (number - 360);
+            }
+            if (isGreaterThan180(number))
+            {
+                number = (360 - number);
+            }
+            return number;
+        }
+        private bool isNegative(decimal number)
+        {
+            if ((number * 2) < number) { return true; }
+            return false;
+        }
+        private bool isGreaterThan360(decimal number)
+        {
+            if (number > 360) { return true; }
+            return false;
+        }
+        private bool isGreaterThan180(decimal number)
+        {
+            if (number > 180) { return true; }
+            return false;
+        }
+    }
+}
---
+++ clockHandAngles/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClockHandApp;
+
+namespace ClockHandApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            talkToUser();
+        }
+
+        static void talkToUser() 
+        {   
+            Console.WriteLine("Please enter the time: ( hh:mm:ss ) ");
+            string time =  Console.ReadLine();
+            Console.WriteLine("\n\n\tThe time is {0}",time);
+
+            Clock c = new Clock();
+            displayAnglesApart(c, time);
+            diplayHandAngles(c, time);
+            Console.ReadLine(); 
+        }
+
+        static void displayAnglesApart(Clock c, string time) 
+        {
+            decimal hourMinutesAngle = 0, minutesSecondsAngle = 0,  hourSecondsAngle = 0;
+            c.getAllAnglesApart(ref hourMinutesAngle, ref minutesSecondsAngle, ref hourSecondsAngle, time);            
+            Console.WriteLine("\nThe Hour and Minute hands are {0} degrees apart.", hourMinutesAngle.ToString("F4"));
+            Console.WriteLine("The Minute and Seconds hands are {0} degrees apart.", minutesSecondsAngle.ToString("F4"));
+            Console.WriteLine("The Hour and Seconds hands are {0} degrees apart.\n", hourSecondsAngle.ToString("F4"));
+        }
+
+        static void diplayHandAngles(Clock c, string time) 
+        { 
+            decimal hourHandAngle=0, minutesHandAngle=0, secondsHandAngle=0;
+            c.getAllHandAngles(ref hourHandAngle, ref minutesHandAngle, ref secondsHandAngle, time);
+            Console.WriteLine("\nThe Hour hand is at {0} degrees from 12.", hourHandAngle.ToString("F4"));
+            Console.WriteLine("The Minute hand is at {0} degrees from 12.", minutesHandAngle.ToString("F4"));
+            Console.WriteLine("The Seconds hand is at {0} degrees from 12.\n", secondsHandAngle.ToString("F4"));
+        }
+    }
+}
